b41da8dd38b7fa13a4083e0b4e430703826f8624
ymtszw <ymtszw@gmail.com>
2018-05-07 05:16:20 +0900

    [cli] Intermediate build

--- docs/myapp/lib/myapp.ex
+++ docs/myapp/lib/myapp.ex
@@ -3,6 +3,21 @@ defmodule Myapp do
   Documentation for Myapp.
   """
 
+  alias Hipchat.ApiClient
+  alias Hipchat.Httpc.Response
+  alias Hipchat.V2.Rooms
+
+  def main([room_id, msg]) do
+    client = ApiClient.new(load_token())
+    {:ok, %Response{status: 201}} = Rooms.send_message(client, room_id, %{message: msg})
+  end
+
+  defp load_token() do
+    Path.expand("~/.config/hipchat_cli/token")
+    |> File.read!()
+    |> String.trim()
+  end
+
   @doc """
   Hello world.
 
--- docs/myapp/mix.exs
+++ docs/myapp/mix.exs
@@ -7,7 +7,8 @@ defmodule Myapp.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: [main_module: Myapp],
     ]
   end
 
@@ -21,7 +22,9 @@ defmodule Myapp.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mix_test_watch, "0.6.0"},
+      {:mix_test_watch, "0.6.0", [only: :dev, runtime: false]},
+      {:hipchat_elixir, "0.2.3"},
+      {:poison, "2.2.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
